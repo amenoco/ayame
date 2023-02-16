3ca2a896dd8c84d6646acbc3e6f3a80f047afba8
EC2 Default User <ec2-user@ip-172-31-41-143.ap-northeast-1.compute.internal>
2023-02-16 16:31:07 +0000

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root to: 'homes#top'
   
 # 顧客用
 # URL /customers/sign_in ...
@@ -16,6 +14,45 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
-  get 'homes/top'
+    ###topの設定
+root to: 'public/homes#top'
+
+ #publicの設定
+scope module: :public do
+    get '/about' => 'homes#about'
+
+    get '/customers/my_page' => 'customers#show', as: 'my_page'
+    get '/customers/infomation/edit' => 'customers#edit', as: 'customers_edit'
+    patch '/customers/infomation' => 'customers#update', as: 'infomation'
+    get '/customers/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
+    patch '/customers/withdraw' => 'customers#withdraw', as: 'withdraw'
+
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all'
+
+    post '/orders/confirm' => 'orders#confirm', as: 'confirm'
+    get '/orders/complete' => 'orders#complete', as: 'complete'
+
+
+    resources :items, :cart_items, :orders, :addresses
+    
+
+  resources :cart_items do
+    collection do
+      delete 'destroy_all'
+    end
+  end
+  end
+
+
+
+#adminの設定
+namespace :admin do
+
+root to: '/admin/homes#top'
+
+
+    resources :items, :genres, :customers, :orders, :orders_details
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
