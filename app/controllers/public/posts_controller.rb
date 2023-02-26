6a4f1e9c2f6381d67c5162cf38c78f5e862819f7
EC2 Default User <ec2-user@ip-172-31-41-143.ap-northeast-1.compute.internal>
2023-02-26 14:04:21 +0000

    マイページルート

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -16,28 +16,40 @@ class Public::PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
     @post = Post.new(post_params)
     @post.user = current_user
-    # binding.irb
-    #byebug
-    @post.save!
-    # if @post.save
-    #   redirect_to posts_path
-    # else
-    #   render :new
-    # end
+    
+    @post.save
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    flash[:notice] ="You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
   
   private
