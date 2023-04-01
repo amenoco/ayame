8cd934d94ab0347bf133be2ee4b2599e8c8720d3
EC2 Default User <ec2-user@ip-172-31-41-143.ap-northeast-1.compute.internal>
2023-04-01 11:43:16 +0000

    noimage画像変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,7 +21,7 @@ class Post < ApplicationRecord
   has_one_attached :profile_image
     def get_profile_image(hight, width)
       unless profile_image.attached?
-        file_path = Rails.root.join('app/assets/images/sample-author1.png')
+        file_path = Rails.root.join('app/assets/images/sample.jpg')
         profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
       end
       profile_image.variant(resize_to_limit: [hight, width]).processed
