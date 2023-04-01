ef72b7236ace52a1c8360524256d0bb25ef82875
EC2 Default User <ec2-user@ip-172-31-41-143.ap-northeast-1.compute.internal>
2023-04-01 16:33:42 +0000

    変更

--- app/controllers/public/categories_controller.rb
+++ app/controllers/public/categories_controller.rb
@@ -1,27 +1,27 @@
-class Category::CategoriesController < PublicController
+class Public::CategoriesController < PublicController
   
-  def new
-  end
+  # def new
+  # end
 
-  def index
-  end
+  # def index
+  # end
 
-  def show
-  end
+  # def show
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def create
-  end
+  # def create
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
   
-  def category_params
-    params.require(:category).permit(:cotent,:category) #categoryを追加
-  end
+  # def category_params
+  #   params.require(:category).permit(:cotent,:category) #categoryを追加
+  # end
 end
